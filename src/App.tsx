1f39ed7ba21abaf6b90461e2d74c6695696aa7e6
bernettorlando <bencube.ben@gmail.com>
2025-12-03 18:55:40 -0800

    Add red border to main container div

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import { MakeRealButton } from './MakeRealButton'
 
 function App() {
     return (
-        <div className="fixed inset-0">
+        <div className="fixed inset-0" style={{ border: '5px solid red', zIndex: 50 }}>
             <Tldraw persistenceKey="ai-whiteboard" licenseKey="tldraw-2026-11-24/WyJBRVgyckZJbSIsWyIqLmFpLXdoaXRlYm9hcmQtbmluZS52ZXJjZWwuYXBwLyJdLDksIjIwMjYtMTEtMjQiXQ.WmzpG7awS7V/0U11Ngz4SjpPosDAXPjL2q+/dHoGzAJFhiul8Qc0Dln2HnhYuzOxI97owAtwFQlSDPATpv/2tw">
                 <MakeRealButton />
             </Tldraw>
